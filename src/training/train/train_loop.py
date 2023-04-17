c6fda814b8cdc43c8038a5ceb34b5075be5bf355
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2023-04-17 12:42:20 +0100

    feat: training with validation

--- src/training/train/train_loop.py
+++ src/training/train/train_loop.py
@@ -1,18 +1,21 @@
-from tqdm import tqdm
 import torch
+from tqdm import tqdm
 from torch.optim import AdamW
+
+from .losses import mse_loss
 
 class TrainLoop(object):
     """
     A trainloop object that handles the training of the model
     """
 
-    def __init__(self, args, model, train_dataloader, validation_dataloader=None):
+    def __init__(self, args, model, train_dataloader, validation_dataloader=None, test_dataloader=None):
 
-        # Save the model and training data
+        # Save the model and training data, validation and test data
         self.model = model
         self.train_dataloader = train_dataloader
         self.validation_dataloader = validation_dataloader
+        self.test_dataloader = test_dataloader
 
         # Setup the number of epochs for the training
         self.epochs = torch.arange(1, args.num_steps + 1)
@@ -56,5 +59,50 @@ class TrainLoop(object):
         # Set the model to be in evaluation mode
         self.model.eval()
 
+    def evaluate(self, dataloader):
+        """
+        Evaluate the performance of the network on a given dataset
+
+        Args:
+            dataloader (Dataloader): A dataloader (i.e. training or validation dataloaders)
+
+        Returns:
+            float: Mean square error over the predictions and the expected output
+        """
+        
+        # Set the model in the evaluation mode
+        self.model.eval()
+
+        y_pred = []
+        y_true = []
+
+        # Disable gradient tracking on this section
+        with torch.no_grad():
+
+            for x, y in dataloader:
+                
+                # Predict the output
+                y_hat = self.model(x)
+                
+                # Add the prediction to the vector
+                y_pred += [y_hat]
+                y_true += [y]
+
+        # Create the torch tensors from the lists (and make sure they are in the right device)
+        y_pred = torch.tensor(y_pred).to(self.model.device)
+        y_true = torch.tensor(y_true).to(self.model.device)
+
+        # Compute the MSE and return its value
+        return mse_loss(y_true, y_pred)
+
+    def validate(self):
+        """
+        Compute the MSE over the validation set
+        """
+        self.evaluate(self.validation_dataloader)
+
     def test(self):
-        pass
+        """
+        Compute the MSE over the test set
+        """
+        self.evaluate(self.test_dataloader)
