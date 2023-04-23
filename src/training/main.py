81442b9a803c2c9250b5d4212288feb60cad2718
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2023-04-23 21:34:22 +0100

    feat: adding a plot file and model used to train during 1000 iterations, after meeting ended. Not the greatest results

--- src/training/main.py
+++ src/training/main.py
@@ -21,6 +21,8 @@ class SuperModelo(nn.Module):
     def __init__(self):
 
         super(SuperModelo, self).__init__()
+
+        # NOTA: adicionar relus no input? no output nao, porque podemos ter valores negativos no output
         
         # Input size and hidden size
         self.input_linear = nn.Linear(3, 100)
@@ -89,7 +91,7 @@ validation_loader = DataLoader(
             collate_fn=lambda x: validation_dataset.collate(x, "cuda"),    # Custom collate function for the dataset
             multiprocessing_context=None)
 
-epochs = torch.arange(0, 2, 1)
+epochs = torch.arange(0, 1000, 1)
 
 
 def save_model(epoch, train_loss, val_loss, save_dir="output/"):
@@ -144,109 +146,111 @@ def compute_loss(y_hat, y, x, target_time):
     return continuity_loss(y_hat, y, target_time) + \
         100 * continuity_last_input_first_output(y_hat, x) + \
         output_continuity(y_hat)
+
+if __name__ == "__main__":
     
-# Train the model
-best_model_idx = 0
-best_val_loss = 1E1000
-for epoch in epochs:
+    # Train the model
+    best_model_idx = 0
+    best_val_loss = 1E1000
+    for epoch in epochs:
 
-    # Enable gradient tracking
-    with torch.enable_grad():
+        # Enable gradient tracking
+        with torch.enable_grad():
 
-        model.train()
+            model.train()
 
-        print('Training epoch {}'.format(epoch))
+            print('Training epoch {}'.format(epoch))
 
-        # Train loss
-        train_loss = []
+            # Train loss
+            train_loss = []
 
-        # Set the model to be in training mode
-        model.train()
+            # Set the model to be in training mode
+            model.train()
 
-        for batch in tqdm(train_loader, desc="Computing batch"):
+            for batch in tqdm(train_loader, desc="Computing batch"):
 
-            # Zero the gradients
-            optimizer.zero_grad()
+                # Zero the gradients
+                optimizer.zero_grad()
 
-            # Get the input of the network and the expect output from the batch
-            x, y = batch
+                # Get the input of the network and the expect output from the batch
+                x, y = batch
 
-            # Perform a forward pass
-            y_hat = model(x, target_time)
+                # Perform a forward pass
+                y_hat = model(x, target_time)
 
-            # --------------------------------------
-            # LOSS
-            # --------------------------------------
-            total_loss = compute_loss(y_hat, y, x, target_time)
+                # --------------------------------------
+                # LOSS
+                # --------------------------------------
+                total_loss = compute_loss(y_hat, y, x, target_time)
 
-            # --------------------------------------
-            train_loss.append(total_loss.item())
+                # --------------------------------------
+                train_loss.append(total_loss.item())
 
-            # Backward pass
-            total_loss.backward()
+                # Backward pass
+                total_loss.backward()
 
-            # Update the parameters
-            optimizer.step()
+                # Update the parameters
+                optimizer.step()
 
-    # Perform validation
-    model.eval()
-    val_loss = []
-    with torch.no_grad():
+        # Perform validation
+        model.eval()
+        val_loss = []
+        with torch.no_grad():
 
-        for x, y in validation_loader:
-            
-            # Perform a forward pass
-            y_hat = model(x, target_time)
+            for x, y in validation_loader:
+                
+                # Perform a forward pass
+                y_hat = model(x, target_time)
 
-            total_val_loss = compute_loss(y_hat, y, x, target_time)
+                total_val_loss = compute_loss(y_hat, y, x, target_time)
 
-            val_loss.append(total_val_loss.item())
-    
-    print("Epoch {} - Train loss: {}".format(epoch, torch.tensor(train_loss).mean()))
-    print("Epoch {} - Validation loss: {}".format(epoch, torch.tensor(val_loss).mean()))
-    writer.add_scalar("Loss/train", torch.tensor(train_loss).mean(), epoch)
-    writer.add_scalar("Loss/validation", torch.tensor(val_loss).mean(), epoch)
+                val_loss.append(total_val_loss.item())
+        
+        print("Epoch {} - Train loss: {}".format(epoch, torch.tensor(train_loss).mean()))
+        print("Epoch {} - Validation loss: {}".format(epoch, torch.tensor(val_loss).mean()))
+        writer.add_scalar("Loss/train", torch.tensor(train_loss).mean(), epoch)
+        writer.add_scalar("Loss/validation", torch.tensor(val_loss).mean(), epoch)
 
-    # Save the model
-    save_model(epoch, torch.tensor(train_loss).mean(), torch.tensor(val_loss).mean())
+        # Save the model
+        save_model(epoch, torch.tensor(train_loss).mean(), torch.tensor(val_loss).mean())
 
-    # Save the best model
-    if torch.tensor(val_loss).mean() < best_val_loss:
-        best_val_loss = torch.tensor(val_loss).mean()
-        best_model_idx = epoch
+        # Save the best model
+        if torch.tensor(val_loss).mean() < best_val_loss:
+            best_val_loss = torch.tensor(val_loss).mean()
+            best_model_idx = epoch
 
-# Save the index of the best model
-save_best_model_idx(best_model_idx, best_val_loss)
+    # Save the index of the best model
+    save_best_model_idx(best_model_idx, best_val_loss)
 
-# Load the best model
-load_best_model(best_model_idx)
+    # Load the best model
+    load_best_model(best_model_idx)
 
-# TODO - Load best model and plot it 
+    # TODO - Load best model and plot it 
 
-    # if epoch >= 1000:
+        # if epoch >= 1000:
 
-    #     print("----")
+        #     print("----")
 
-    #     for i in range(len(train_dataset)):
+        #     for i in range(len(train_dataset)):
 
-    #         x, y = train_dataset[i]
+        #         x, y = train_dataset[i]
 
-    #         y_hat = model(x[None, :, :], target_time)
-            
-    #         print(y_hat.shape)
-    #         time = torch.arange(0, x.shape[0] + y.shape[0]).numpy(force=True)
+        #         y_hat = model(x[None, :, :], target_time)
+                
+        #         print(y_hat.shape)
+        #         time = torch.arange(0, x.shape[0] + y.shape[0]).numpy(force=True)
 
-    #         # Plot the resulting prediction
-    #         plt.figure()
-    #         plt.plot(time, torch.cat((x[:, 0], y[:, 0]), dim=0).numpy(force=True), label="x")        # x
-    #         plt.plot(time, torch.cat((x[:, 1], y[:, 1]), dim=0).numpy(force=True), label="y")        # y
-    #         plt.plot(time, torch.cat((x[:, 2], y[:, 2]), dim=0).numpy(force=True), label="z")        # z
+        #         # Plot the resulting prediction
+        #         plt.figure()
+        #         plt.plot(time, torch.cat((x[:, 0], y[:, 0]), dim=0).numpy(force=True), label="x")        # x
+        #         plt.plot(time, torch.cat((x[:, 1], y[:, 1]), dim=0).numpy(force=True), label="y")        # y
+        #         plt.plot(time, torch.cat((x[:, 2], y[:, 2]), dim=0).numpy(force=True), label="z")        # z
 
-    #         time2 = torch.arange(x.shape[0], x.shape[0] + y_hat.shape[1]).numpy(force=True)
-    #         plt.plot(time2, y_hat[0, :, 0].numpy(force=True), label="x_hat")        # x
-    #         plt.plot(time2, y_hat[0, :, 1].numpy(force=True), label="y_hat")        # y
-    #         plt.plot(time2, y_hat[0, :, 2].numpy(force=True), label="z_hat")        # z
+        #         time2 = torch.arange(x.shape[0], x.shape[0] + y_hat.shape[1]).numpy(force=True)
+        #         plt.plot(time2, y_hat[0, :, 0].numpy(force=True), label="x_hat")        # x
+        #         plt.plot(time2, y_hat[0, :, 1].numpy(force=True), label="y_hat")        # y
+        #         plt.plot(time2, y_hat[0, :, 2].numpy(force=True), label="z_hat")        # z
 
-    #         plt.legend()
-    #         plt.show()
+        #         plt.legend()
+        #         plt.show()
 
---
+++ src/training/plot.py
@@ -0,0 +1,37 @@
+import torch
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from data_loaders.mocap_swipe_loader import MocapSwipeLoader
+
+from main import model, load_best_model, train_dataset, target_time
+
+# Load the model
+target_time = 25
+
+best_model = torch.load("output/best_model_idx.pth.tar")
+best_model_idx = best_model["best_model_idx"]
+
+load_best_model(best_model_idx)
+
+for i in range(len(train_dataset)):
+
+    x, y = train_dataset[i]
+
+    y_hat = model(x[None, :, :], target_time)
+    
+    print(y_hat.shape)
+    time = torch.arange(0, x.shape[0] + y.shape[0]).numpy(force=True)
+
+    # Plot the resulting prediction
+    plt.figure()
+    plt.plot(time, torch.cat((x[:, 0], y[:, 0]), dim=0).numpy(force=True), label="x")        # x
+    plt.plot(time, torch.cat((x[:, 1], y[:, 1]), dim=0).numpy(force=True), label="y")        # y
+    plt.plot(time, torch.cat((x[:, 2], y[:, 2]), dim=0).numpy(force=True), label="z")        # z
+
+    time2 = torch.arange(x.shape[0], x.shape[0] + y_hat.shape[1]).numpy(force=True)
+    plt.plot(time2, y_hat[0, :, 0].numpy(force=True), label="x_hat")        # x
+    plt.plot(time2, y_hat[0, :, 1].numpy(force=True), label="y_hat")        # y
+    plt.plot(time2, y_hat[0, :, 2].numpy(force=True), label="z_hat")        # z
+
+    plt.legend()
+    plt.show()
