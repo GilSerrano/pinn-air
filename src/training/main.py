a7bc80fa72bf4211e359acee3b44e683c3e2ddee
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2023-04-23 20:00:41 +0100

    feat 2: training with all the data

--- src/training/main.py
+++ src/training/main.py
@@ -9,6 +9,9 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 
+from torch.utils.tensorboard import SummaryWriter
+
+
 class SuperModelo(nn.Module):
 
     def __init__(self):
@@ -17,26 +20,28 @@ class SuperModelo(nn.Module):
         
         # Input size and hidden size
         self.input_linear = nn.Linear(3, 100)
-        self.lstm = nn.LSTM(100, 15, batch_first=True)
-        self.linear = nn.Linear(15, 100)
+        self.input_linear2 = nn.Linear(100, 200)
+        self.input_linear3 = nn.Linear(200, 300)
 
+        self.lstm = nn.LSTM(300, 15, batch_first=True)
+        self.linear = nn.Linear(15, 300)
 
-        self.linear_out = nn.Linear(100, 3)
+        self.linear_out = nn.Linear(300, 3)
 
     def forward(self, x, target_time):
 
         outputs = torch.zeros(x.shape[0], target_time, x.shape[2]).to("cpu")
 
         x = self.input_linear(x)
+        x = self.input_linear2(x)
+        x = self.input_linear3(x)
 
         # Pass the previous sequence through the lstm
         lstm_out, hidden = self.lstm(x)
         lstm_out = self.linear(lstm_out[:, -1, :])
         lstm_out = lstm_out[:,None,:]
-        
         output = self.linear_out(lstm_out)
-        outputs[:,0,:] = output
-        
+        outputs[:,0,:] = output[:,-1,:]
 
         # Predict the next sequence recursively
         for i in range(1, target_time):
@@ -53,7 +58,9 @@ class SuperModelo(nn.Module):
 model = SuperModelo().to("cpu")
 optimizer = AdamW(model.parameters(), lr=1E-4, weight_decay=0.05)
 
-target_time = 50
+writer = SummaryWriter(log_dir="output")
+
+target_time = 25
 
 # Load the dataset
 train_dataset = MocapSwipeLoader(input_window=50, output_window=target_time, stride=1, split="train", device="cpu")
@@ -70,6 +77,20 @@ train_loader = DataLoader(
 epochs = torch.arange(0, 650, 1)
 
 
+def continuity_loss(y_hat, y, target_time):
+    exp_decay = 10 * torch.exp(-0.1*torch.arange(0, target_time, 1))
+    return torch.sum((torch.norm(y_hat - y, dim=2) ** 2) * exp_decay)
+
+def continuity_last_input_first_output(y_hat, x):
+    return torch.sum(torch.norm(y_hat[:, 0, :] - x[:, -1, :], dim=1) **2, dim=0)
+
+def output_continuity(y_hat):
+
+    total_loss = 0
+    for t in range(y.shape[1] - 1):
+        total_loss += torch.sum(torch.norm(y_hat[:, t, :] - y_hat[:, t+1, :], dim=1) ** 2, dim=0)
+
+    return total_loss
 
 # Enable gradient tracking
 with torch.enable_grad():
@@ -81,7 +102,6 @@ with torch.enable_grad():
 
         # Train loss
         train_loss = []
-        train_individual_losses = []
 
         # Set the model to be in training mode
         model.train()
@@ -92,20 +112,27 @@ with torch.enable_grad():
             optimizer.zero_grad()
 
             # Get the input of the network and the expect output from the batch
-            x, y = train_dataset[0]
-            x = x.unsqueeze(0)
-            y = y.unsqueeze(0)
+            x, y = batch
 
             # Perform a forward pass
             y_hat = model(x, target_time)
 
-            # Forward pass
+            # --------------------------------------
+            # LOSS
+            # --------------------------------------
             total_loss = 0
-            for elem in range(x.shape[0]):     # (batch, time, features)
-                # For each feature
-                for i in range(x.shape[2]):
-                    loss = F.mse_loss(y_hat[elem, :, i], y[elem, :, i])
-                    total_loss += loss
+
+            # loss term continuity between the outputs
+            total_loss += continuity_loss(y_hat, y, target_time)
+
+            # loss term continuity with the last input and the first output
+            total_loss += continuity_last_input_first_output(y_hat, x)
+
+            # loss term continuity between the outputs
+            total_loss += output_continuity(y_hat)
+
+            # --------------------------------------
+            train_loss.append(total_loss.item())
 
             # Backward pass
             total_loss.backward()
@@ -113,7 +140,10 @@ with torch.enable_grad():
             # Update the parameters
             optimizer.step()
 
-            print(total_loss.item())
+            #print(total_loss.item())
+        
+        print("Epoch {} - Train loss: {}".format(epoch, torch.tensor(train_loss).mean()))
+        writer.add_scalar("Loss/train", torch.tensor(train_loss).mean(), epoch)
 
         if epoch >= 600:
 
