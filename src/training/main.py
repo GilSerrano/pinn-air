fix: bug in the quaternion convention

src/training/main.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 
 from utils.fix_seed import fix_seed
 
+from dynamics.utils import quaternion_multiply, quaternion_invert
+
 
 from torch.utils.tensorboard import SummaryWriter
 
@@ -75,7 +77,7 @@ torch.set_default_device("cuda")
 model = SuperModelo().to("cuda")
 optimizer = AdamW(model.parameters(), lr=1E-4, weight_decay=0.05)
 
-writer = SummaryWriter(log_dir="output4")
+writer = SummaryWriter(log_dir="output5")
 
 target_time = 25
 
@@ -104,7 +106,7 @@ validation_loader = DataLoader(
 epochs = torch.arange(0, 600, 1)
 
 
-def save_model(epoch, train_loss, val_loss, save_dir="output4/"):
+def save_model(epoch, train_loss, val_loss, save_dir="output5/"):
 
     checkpoint_path = os.path.join(save_dir, 'checkpoint_{:04d}.pth.tar'.format(epoch))
     print('Saving checkpoint {}'.format(checkpoint_path))
@@ -118,7 +120,7 @@ def save_model(epoch, train_loss, val_loss, save_dir="output4/"):
         "optimizer": optimizer.state_dict()
     }, checkpoint_path)
 
-def save_best_model_idx(epoch, val_loss, save_dir="output4/"):
+def save_best_model_idx(epoch, val_loss, save_dir="output5/"):
 
     checkpoint_path = os.path.join(save_dir, 'best_model_idx.pth.tar')
     print('Saving checkpoint {}'.format(checkpoint_path))
@@ -126,7 +128,7 @@ def save_best_model_idx(epoch, val_loss, save_dir="output4/"):
     # Save the current model
     torch.save({"best_model_idx": best_model_idx, "best_val_loss": best_val_loss,}, checkpoint_path)
 
-def load_best_model(epoch, save_dir="output4/"):
+def load_best_model(epoch, save_dir="output5/"):
     
     checkpoint_path = os.path.join(save_dir, 'checkpoint_{:04d}.pth.tar'.format(epoch))
     print("Loading: {}".format(checkpoint_path))
@@ -135,10 +137,42 @@ def load_best_model(epoch, save_dir="output4/"):
     model.load_state_dict(checkpoint['model'])
     optimizer.load_state_dict(checkpoint['optimizer'])
 
+#def continuity_loss(y_hat, y, target_time):
+#    exp_decay = 10 * torch.exp(-0.1*torch.arange(0, target_time, 1)).to("cuda")
+#    return torch.sum((torch.norm(y_hat - y, dim=2) ** 2) * exp_decay)
 
-def continuity_loss(y_hat, y, target_time):
+def position_error(y_hat, y, target_time):
+    
+    # Compute the exponential decay
     exp_decay = 10 * torch.exp(-0.1*torch.arange(0, target_time, 1)).to("cuda")
-    return torch.sum((torch.norm(y_hat - y, dim=2) ** 2) * exp_decay)
+    
+    # Compute the discounted position error
+    return torch.sum((torch.norm(y_hat[:, :, 0:3] - y[:, :, 0:3], dim=2) ** 2) * exp_decay)
+
+def velocity_error(y_hat, y, target_time):
+
+    # Compute the exponential decay
+    exp_decay = 10 * torch.exp(-0.1*torch.arange(0, target_time, 1)).to("cuda")
+
+    # Compute the discounted velocity error
+    return torch.sum((torch.norm(y_hat[:, :, 3:6] - y[:, :, 3:6], dim=2) ** 2) * exp_decay)
+
+def quaternion_error(y_hat, y, target_time):
+
+    # Compute the exponential decay
+    exp_decay = 10 * torch.exp(-0.1*torch.arange(0, target_time, 1)).to("cuda")
+
+    # Compute the discounted quaternion error
+    identity_quaternion = torch.tensor([1.0, 0.0, 0.0, 0.0]).to("cuda")
+
+    return torch.sum((torch.norm(quaternion_multiply(y[..., 6:10], quaternion_invert(y_hat[..., 6:10])) - identity_quaternion, dim=2) ** 2) * exp_decay)
+
+def quaternion_norm(y_hat):
+
+    quat_norm = y_hat[..., 6:10].norm(p=2, dim=-1)
+    one = torch.ones_like(quat_norm)
+    
+    return torch.sum((quat_norm - one) ** 2)
 
 def continuity_last_input_first_output(y_hat, x):
     return torch.sum(torch.norm(y_hat[:, 0, :] - x[:, -1, :], dim=1) **2, dim=0)
@@ -153,9 +187,13 @@ def output_continuity(y_hat):
 
 def compute_loss(y_hat, y, x, target_time):
 
-    return continuity_loss(y_hat, y, target_time) + \
+    return position_error(y_hat, y, target_time) + \
+        velocity_error(y_hat, y, target_time) + \
         100 * continuity_last_input_first_output(y_hat, x) + \
-        output_continuity(y_hat)
+        output_continuity(y_hat) + \
+        100 * quaternion_norm(y_hat) + \
+        quaternion_error(y_hat, y, target_time)
+    
 
 if __name__ == "__main__":
     
@@ -229,8 +267,8 @@ if __name__ == "__main__":
             best_val_loss = torch.tensor(val_loss).mean()
             best_model_idx = epoch
 
-    # Save the index of the best model
-    save_best_model_idx(best_model_idx, best_val_loss)
+            # Save the index of the best model
+            save_best_model_idx(best_model_idx, best_val_loss)
 
     # Load the best model
     load_best_model(best_model_idx)

src/training/plot.py
@@ -8,10 +8,10 @@ from main import model, load_best_model, train_dataset, target_time
 # Load the model
 target_time = 25
 
-best_model = torch.load("output4/best_model_idx.pth.tar")
+best_model = torch.load("output5/best_model_idx.pth.tar")
 best_model_idx = best_model["best_model_idx"]
 
-load_best_model(best_model_idx, save_dir="output4/")
+load_best_model(best_model_idx, save_dir="output5/")
 
 
 # Plot the quaternion of the vehicle
@@ -26,16 +26,18 @@ load_best_model(best_model_idx, save_dir="output4/")
 
 #     # Plot the resulting prediction
 #     plt.figure()
-#     plt.plot(time, torch.cat((x[:, 6], y[:, 6]), dim=0).numpy(force=True), label="x")        # qx
-#     plt.plot(time, torch.cat((x[:, 7], y[:, 7]), dim=0).numpy(force=True), label="y")        # qy
-#     plt.plot(time, torch.cat((x[:, 8], y[:, 8]), dim=0).numpy(force=True), label="z")        # qz
-#     plt.plot(time, torch.cat((x[:, 9], y[:, 9]), dim=0).numpy(force=True), label="z")        # qw
+#     plt.plot(time, torch.cat((x[:, 6], y[:, 6]), dim=0).numpy(force=True), label="qw")        # qx
+#     plt.plot(time, torch.cat((x[:, 7], y[:, 7]), dim=0).numpy(force=True), label="qx")        # qy
+#     plt.plot(time, torch.cat((x[:, 8], y[:, 8]), dim=0).numpy(force=True), label="qy")        # qz
+#     plt.plot(time, torch.cat((x[:, 9], y[:, 9]), dim=0).numpy(force=True), label="qz")        # qw
+
+#     #plt.plot(time, torch.cat((torch.norm(x[:,6:10], p=2, dim=-1), torch.norm(y[:,6:10], p=2, dim=-1)), dim=0).numpy(force=True), label="norm")
 
 #     time2 = torch.arange(x.shape[0], x.shape[0] + y_hat.shape[1]).numpy(force=True)
-#     plt.plot(time2, y_hat[0, :, 6].numpy(force=True), label="x_hat")        # qx
-#     plt.plot(time2, y_hat[0, :, 7].numpy(force=True), label="y_hat")        # qy
-#     plt.plot(time2, y_hat[0, :, 8].numpy(force=True), label="z_hat")        # qz
-#     plt.plot(time2, y_hat[0, :, 9].numpy(force=True), label="z_hat")        # qw
+#     plt.plot(time2, y_hat[0, :, 6].numpy(force=True), label="qw_hat")        # qx
+#     plt.plot(time2, y_hat[0, :, 7].numpy(force=True), label="qx_hat")        # qy
+#     plt.plot(time2, y_hat[0, :, 8].numpy(force=True), label="qy_hat")        # qz
+#     plt.plot(time2, y_hat[0, :, 9].numpy(force=True), label="qz_hat")        # qw
 
 #     plt.legend()
 #     plt.show()
@@ -65,28 +67,28 @@ load_best_model(best_model_idx, save_dir="output4/")
 #     plt.show()
 
 # Plot the position of the vehicle
-# for i in range(len(train_dataset)):
+for i in range(len(train_dataset)):
 
-#     x, y = train_dataset[i]
+    x, y = train_dataset[i]
 
-#     y_hat = model(x[None, :, :], target_time)
+    y_hat = model(x[None, :, :], target_time)
     
-#     print(y_hat.shape)
-#     time = torch.arange(0, x.shape[0] + y.shape[0]).numpy(force=True)
+    print(y_hat.shape)
+    time = torch.arange(0, x.shape[0] + y.shape[0]).numpy(force=True)
 
-#     # Plot the resulting prediction
-#     plt.figure()
-#     plt.plot(time, torch.cat((x[:, 0], y[:, 0]), dim=0).numpy(force=True), label="x")        # x
-#     plt.plot(time, torch.cat((x[:, 1], y[:, 1]), dim=0).numpy(force=True), label="y")        # y
-#     plt.plot(time, torch.cat((x[:, 2], y[:, 2]), dim=0).numpy(force=True), label="z")        # z
+    # Plot the resulting prediction
+    plt.figure()
+    plt.plot(time, torch.cat((x[:, 0], y[:, 0]), dim=0).numpy(force=True), label="x")        # x
+    plt.plot(time, torch.cat((x[:, 1], y[:, 1]), dim=0).numpy(force=True), label="y")        # y
+    plt.plot(time, torch.cat((x[:, 2], y[:, 2]), dim=0).numpy(force=True), label="z")        # z
 
-#     time2 = torch.arange(x.shape[0], x.shape[0] + y_hat.shape[1]).numpy(force=True)
-#     plt.plot(time2, y_hat[0, :, 0].numpy(force=True), label="x_hat")        # x
-#     plt.plot(time2, y_hat[0, :, 1].numpy(force=True), label="y_hat")        # y
-#     plt.plot(time2, y_hat[0, :, 2].numpy(force=True), label="z_hat")        # z
+    time2 = torch.arange(x.shape[0], x.shape[0] + y_hat.shape[1]).numpy(force=True)
+    plt.plot(time2, y_hat[0, :, 0].numpy(force=True), label="x_hat")        # x
+    plt.plot(time2, y_hat[0, :, 1].numpy(force=True), label="y_hat")        # y
+    plt.plot(time2, y_hat[0, :, 2].numpy(force=True), label="z_hat")        # z
 
-#     plt.legend()
-#     plt.show()
+    plt.legend()
+    plt.show()
 
 # Plot the position of the load
 # for i in range(len(train_dataset)):
@@ -113,27 +115,27 @@ load_best_model(best_model_idx, save_dir="output4/")
 #     plt.show()
 
 # Plot the input of the vehicle
-for i in range(len(train_dataset)):
+# for i in range(len(train_dataset)):
 
-    x, y = train_dataset[i]
+#     x, y = train_dataset[i]
 
-    y_hat = model(x[None, :, :], target_time)
+#     y_hat = model(x[None, :, :], target_time)
     
-    print(y_hat.shape)
-    time = torch.arange(0, x.shape[0] + y.shape[0]).numpy(force=True)
+#     print(y_hat.shape)
+#     time = torch.arange(0, x.shape[0] + y.shape[0]).numpy(force=True)
 
-    # Plot the resulting prediction
-    plt.figure()
-    plt.plot(time, torch.cat((x[:, 13], y[:, 13]), dim=0).numpy(force=True), label="x")        # qx
-    plt.plot(time, torch.cat((x[:, 14], y[:, 14]), dim=0).numpy(force=True), label="y")        # qy
-    plt.plot(time, torch.cat((x[:, 15], y[:, 15]), dim=0).numpy(force=True), label="z")        # qz
-    plt.plot(time, torch.cat((x[:, 16], y[:, 16]), dim=0).numpy(force=True), label="z")        # qw
+#     # Plot the resulting prediction
+#     plt.figure()
+#     plt.plot(time, torch.cat((x[:, 13], y[:, 13]), dim=0).numpy(force=True), label="x")        # qx
+#     plt.plot(time, torch.cat((x[:, 14], y[:, 14]), dim=0).numpy(force=True), label="y")        # qy
+#     plt.plot(time, torch.cat((x[:, 15], y[:, 15]), dim=0).numpy(force=True), label="z")        # qz
+#     plt.plot(time, torch.cat((x[:, 16], y[:, 16]), dim=0).numpy(force=True), label="z")        # qw
 
-    time2 = torch.arange(x.shape[0], x.shape[0] + y_hat.shape[1]).numpy(force=True)
-    plt.plot(time2, y_hat[0, :, 13].numpy(force=True), label="x_hat")        # qx
-    plt.plot(time2, y_hat[0, :, 14].numpy(force=True), label="y_hat")        # qy
-    plt.plot(time2, y_hat[0, :, 15].numpy(force=True), label="z_hat")        # qz
-    plt.plot(time2, y_hat[0, :, 16].numpy(force=True), label="z_hat")        # qw
+#     time2 = torch.arange(x.shape[0], x.shape[0] + y_hat.shape[1]).numpy(force=True)
+#     plt.plot(time2, y_hat[0, :, 13].numpy(force=True), label="x_hat")        # qx
+#     plt.plot(time2, y_hat[0, :, 14].numpy(force=True), label="y_hat")        # qy
+#     plt.plot(time2, y_hat[0, :, 15].numpy(force=True), label="z_hat")        # qz
+#     plt.plot(time2, y_hat[0, :, 16].numpy(force=True), label="z_hat")        # qw
 
-    plt.legend()
-    plt.show()
+#     plt.legend()
+#     plt.show()