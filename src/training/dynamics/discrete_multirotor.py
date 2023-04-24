fix: bug in discrete multirotor model on the rotation matrix integration

src/training/dynamics/discrete_multirotor.py
@@ -49,27 +49,11 @@ class DiscreteMultirotor:
              x=(batch, time_series_length, len(state))
              u=(batch, time_series_length, len(input))
 
-             sequencia de input
-             4000 5 17
-
-             output da rede
-             4000 1 13
-
-              x[0], x[1], x[2], x[3], x[4] -> x[1], x[2], x[3], x[4], x[5]
-
-              x = 4000 5 13
-              u = 4000 5 4     -> 4000 5 13
-
-              (4000 1 13) - (:, -1, :)
-
-            output 
-            4000 5 14
-
         Args:
             x (torch.Tensor): The state of the system at the previous time-step, i.e. x[k-1]
-                x[k-1] = [x,y,z | v_x, v_y,v_z | q_x, q_y, q_z, q_w]
+                x[k-1] = [x,y,z | v_x, v_y,v_z | q_w, q_x, q_y, q_z]
             u (torch.Tensor): The input of the system at the current time-step, i.e. u[k]
-                u[k] = [w_x, w_y, w_z | T]  
+                u[k] = [w_x, w_y, w_z | T]
         Returns:
             torch.Tensor: The state of the system at the current time-step, i.e. x[k]
         """
@@ -79,12 +63,14 @@ class DiscreteMultirotor:
         # -----------------------------------------------------
         
         # 1) Convert the quaternion to a rotation matrix (this can be simplified) 
-        # Note that we receive the quaternion in the [qx, qx, qy, qw] convention
+        # Note that we receive the quaternion in the [qw, qx, qx, qy] convention
         rot = quaternion_to_matrix(x[..., 6:10])
 
         # Generate the thrust vector (in order to handle batches and sequences)
         u_shape = list(u.shape[:-1]) + [3]                  # (batch_size, sequence_size, 3)
+
         total_thrust = torch.zeros(size=u_shape)
+
         total_thrust[..., 2] = u[..., 3]                    # Vectors of the type (batch_size, time_sequence, [0.0, 0.0, total_thrust])
 
         # 2) Compute the reference linear acceleration that the linear system is supposed to track
@@ -104,10 +90,10 @@ class DiscreteMultirotor:
         skew = skew_symmetric(u[..., 0:3])
 
         # 2) Compute the new rotation matrix R[k+1]
-        new_R = rot * torch.matrix_exp(self.Ts * skew)
+        new_R = torch.matmul(rot, torch.matrix_exp(self.Ts * skew))
 
         # 3) Generate the new quaternion from the rotation matrix
-        # Note that we produce a quaternion in the [qx, qy, qz, qw] convention
+        # Note that we produce a quaternion in the [qw, qx, qy, qz] convention
         new_q = matrix_to_quaternion(new_R)
 
         # -----------------------------------------------------

src/training/main.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 from utils.fix_seed import fix_seed
 
 from dynamics.utils import quaternion_multiply, quaternion_invert
+from dynamics.discrete_multirotor import DiscreteMultirotor
 
 
 from torch.utils.tensorboard import SummaryWriter
@@ -76,6 +77,8 @@ class SuperModelo(nn.Module):
 torch.set_default_device("cuda")
 model = SuperModelo().to("cuda")
 optimizer = AdamW(model.parameters(), lr=1E-4, weight_decay=0.05)
+Ts = 0.03
+physics_model = DiscreteMultirotor(Ts, 1.0, "cuda")
 
 writer = SummaryWriter(log_dir="output5")
 
@@ -174,6 +177,25 @@ def quaternion_norm(y_hat):
     
     return torch.sum((quat_norm - one) ** 2)
 
+def physics_error(y_hat, x, y, target_time, physics_model):
+
+    # Create an empty tensor to store the predictions
+    physics_pred = torch.zeros(x.shape[0], target_time, 10).to("cuda")
+
+    # Compute the first prediction of the physical model
+    #physics_pred[:, 0, :] = physics_model.run(x=x[:, -1, 0:10].unsqueeze(1), u=x[:,-1, 13:17].unsqueeze(1))
+
+    # Compute the rest of the predictions of the physical model
+    physics_pred[:,1:,:] = physics_model.run(x=y[..., 0:10], u=y[..., 13:17])
+
+    # Compute the error between the predicted and the physical model
+    pos_error = position_error(y_hat, physics_pred, target_time)
+    vel_error = velocity_error(y_hat, physics_pred, target_time)
+    quat_error = quaternion_error(y_hat, physics_pred, target_time)
+
+    return pos_error + vel_error + quat_error
+
+
 def continuity_last_input_first_output(y_hat, x):
     return torch.sum(torch.norm(y_hat[:, 0, :] - x[:, -1, :], dim=1) **2, dim=0)
 
@@ -185,14 +207,15 @@ def output_continuity(y_hat):
 
     return total_loss
 
-def compute_loss(y_hat, y, x, target_time):
+def compute_loss(y_hat, y, x, target_time, physics_model):
 
     return position_error(y_hat, y, target_time) + \
         velocity_error(y_hat, y, target_time) + \
         100 * continuity_last_input_first_output(y_hat, x) + \
         output_continuity(y_hat) + \
         100 * quaternion_norm(y_hat) + \
-        quaternion_error(y_hat, y, target_time)
+        quaternion_error(y_hat, y, target_time) + \
+        physics_error(y_hat, x, y, target_time, physics_model)
     
 
 if __name__ == "__main__":
@@ -229,7 +252,7 @@ if __name__ == "__main__":
                 # --------------------------------------
                 # LOSS
                 # --------------------------------------
-                total_loss = compute_loss(y_hat, y, x, target_time)
+                total_loss = compute_loss(y_hat, y, x, target_time, physics_model)
 
                 # --------------------------------------
                 train_loss.append(total_loss.item())
@@ -250,7 +273,7 @@ if __name__ == "__main__":
                 # Perform a forward pass
                 y_hat = model(x, target_time)
 
-                total_val_loss = compute_loss(y_hat, y, x, target_time)
+                total_val_loss = compute_loss(y_hat, y, x, target_time, physics_model)
 
                 val_loss.append(total_val_loss.item())
         